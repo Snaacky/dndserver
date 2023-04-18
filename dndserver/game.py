c37752aac777b6a955cb870f1ca59f8c19f81c7a
Snaacky <snaacky@pm.me>
2023-04-18 18:15:59 -0400

    Removed deprecated code, added network transport wrappers

--- dndserver/game.py
+++ dndserver/game.py
@@ -1,7 +1,7 @@
 from loguru import logger
 from twisted.internet.protocol import Factory, Protocol
 
-
+from dndserver import database
 from dndserver.handlers import login
 from dndserver.handlers import character
 from dndserver.protos import _PacketCommand_pb2 as pc
@@ -13,29 +13,27 @@ class GameFactory(Factory):
 
 
 class GameProtocol(Protocol):
-    def __init__(self) -> None:
-        super().__init__()
-        self.packet_header= b"\x1e\x00\x00\x00\x0c\x00\x00\x00"
-        
-        
     def connectionMade(self):
         logger.debug("Connection made")
 
     def dataReceived(self, data: bytes):
         logger.debug(f"Received {pc.PacketCommand.Name(data[4])}")
-        print(f"Received ", data)
         match pc.PacketCommand.Name(data[4]):
             # TODO: Can we access these enums directly? 'EnumTypeWrapper' object is not callable
             case "C2S_ALIVE_REQ":
-                logger.debug(data)
-                self.transport.write(pc.SS2C_ALIVE_RES().SerializeToString())
-                
+                self.send(pc.SS2C_ALIVE_RES().SerializeToString())
             case "C2S_ACCOUNT_LOGIN_REQ":
-                self.transport.write(login.process_login(self, data))
-
+                self.send(login.process_login(self, data))
+            case "C2S_ACCOUNT_CHARACTER_LIST_REQ":
+                self.send(character.list_characters(self, data))
             case "C2S_ACCOUNT_CHARACTER_CREATE_REQ":
-
-                self.transport.write(character.process_character(self, data))
-                logger.debug(f"Received {pc.PacketCommand.Name(data[4])}")
+                self.send(character.create_character(self, data))
             case _:
                 logger.error(f"Received {pc.PacketCommand.Name(data[4])} {data} packet but no handler yet")
+
+    def send(self, data: bytes):
+        self.transport.write(data)
+
+    def send_many(self, packets: list):
+        for packet in packets:
+            self.transport.write(packet)
