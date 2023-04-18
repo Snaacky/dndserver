40524aab790319b50124755a761aa48e25e6d5c6
Snaacky <snaacky@pm.me>
2023-04-17 21:19:42 -0400

    Added database schema for character creation

--- dndserver/database.py
+++ dndserver/database.py
@@ -19,8 +19,15 @@ def setup():
     db = get()
     if "users" not in db:
         users = db.create_table("users")
-        users.create_column("username", db.types.text)  # 2 characters, 20 characters
+        users.create_column("username", db.types.text)  # 2 characters min, 20 characters max
         users.create_column("password", db.types.text)
         users.create_column("hwids", db.types.text)
         users.create_column("build_version", db.types.text)
         users.create_column("is_banned", db.types.integer)  # 12 = ban user, 13 = ban cheater, 14 = ban inappropriate name, 15 = ban etc, 16 = hwid ban
+
+    if "characters" not in db:
+        chars = db.create_table("characters")
+        chars.create_column("owner_id", db.types.integer)
+        chars.create_column("name", db.types.text)  # ?? characters min, 20 characters max
+        chars.create_column("sex", db.types.text)
+        chars.create_column("class", db.types.text)
