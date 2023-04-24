ade6a13c58af8c18970ab79f5b195d68d4182482
itzandroidtab <koen@itzandroidtab.nl>
2023-04-25 00:48:17 +0200

    added packet splitting when receiving multiple packets in 1 transmission

--- dndserver/handlers/menu.py
+++ dndserver/handlers/menu.py
@@ -6,14 +6,8 @@ def process_location(ctx, msg):
     # get the location from the message.
     req = SC2S_META_LOCATION_REQ()
 
-    # TODO: get a better way to get the actual size of the proto as it also has the 
-    # size of the other packet. Currently hardcoded to 2 bytes
-    req.ParseFromString(msg[:2])
-
-    # check if we have more data than expected for the proto
-    if len(msg) > 2:
-        # process the other data as well
-        ctx.dataReceived(msg[2:])
+    # get the location from the string
+    req.ParseFromString(msg)
 
     # respond to the location request
     return SS2C_META_LOCATION_RES(location=req.location)
