d573de557c87ba8f4766c0f5e799e1848bf9c1b3
Snaacky <snaacky@pm.me>
2023-04-24 18:17:54 -0400

    Fixed missing comma after alphabetization

--- dndserver/protocol.py
+++ dndserver/protocol.py
@@ -46,9 +46,9 @@ class GameProtocol(Protocol):
             pc.C2S_FRIEND_FIND_REQ: friends.find_user,
             pc.C2S_PARTY_INVITE_REQ: friends.party_invite,
             pc.C2S_META_LOCATION_REQ: menu.process_location,
-            pc.C2S_MERCHANT_LIST_REQ: merchant.get_merchant_list
+            pc.C2S_MERCHANT_LIST_REQ: merchant.get_merchant_list,
             pc.C2S_TRADE_MEMBERSHIP_REQUIREMENT_REQ: trade.get_trade_reqs,
-            pc.C2S_TRADE_MEMBERSHIP_REQ: trade.process_membership,
+            pc.C2S_TRADE_MEMBERSHIP_REQ: trade.process_membership
         }
         handler = [k for k in handlers.keys() if k == _id]
         if not handler:
