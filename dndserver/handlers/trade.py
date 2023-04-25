278363e7a980d5516baa1fb102b7e39d457a960d
Krofty <rwignall0@gmail.com>
2023-04-25 09:21:29 +0100

    Refactor plus additional chat features added

--- dndserver/handlers/trade.py
+++ dndserver/handlers/trade.py
@@ -1,5 +1,3 @@
-import random
-
 from dndserver.protos import PacketCommand as pc
 from dndserver.protos.Trade import (SS2C_TRADE_MEMBERSHIP_REQUIREMENT_RES, STRADE_MEMBERSHIP_REQUIREMENT,
                                     SS2C_TRADE_MEMBERSHIP_RES, SS2C_TRADE_CHANNEL_LIST_RES, STRADE_CHANNEL,
@@ -7,8 +5,7 @@ from dndserver.protos.Trade import (SS2C_TRADE_MEMBERSHIP_REQUIREMENT_RES, STRAD
                                     SC2S_TRADE_CHANNEL_CHAT_REQ)
 from dndserver.protos.Chat import (SCHATDATA, SCHATDATA_PIECE, SCHATDATA_PIECE_ITEM, 
                                    SCHATDATA_PIECE_ITEM_PROPERTY)
-from dndserver.protos.Character import SACCOUNT_NICKNAME, SCHARACTER_INFO
-
+from dndserver.protos.Character import SACCOUNT_NICKNAME
 
 
 def get_trade_reqs(ctx, msg):
@@ -26,53 +23,44 @@ def select_trade_channel(ctx, msg):
     return SS2C_TRADE_CHANNEL_SELECT_RES(result=pc.SUCCESS)
 
 
-def chat_piece_item_property():
-    return SCHATDATA_PIECE_ITEM_PROPERTY(pid="1", pv=1)
-
-
-def chat_piece_item():
-    return SCHATDATA_PIECE_ITEM(uid=1, iid="1")
-
-
-def chat_piece(chatmsg):
-    data = SCHATDATA_PIECE()
-    data.chatStr = chatmsg
-    #data.chatDataPieceItem.CopyFrom(chat_piece_item())
-    return data
-
-
-def chat_data(chatmsg):
-    nickname = SACCOUNT_NICKNAME(
-        originalNickName="krofty",
-        streamingModeNickName=f"Fighter#{random.randrange(1000000, 1700000)}"
-    )
-
-    data = SCHATDATA()
-    data.accountId = "1"
-    data.characterId = "1"
-    data.nickname.CopyFrom(nickname)
-    data.partyId = "1"
-    data.chatDataPieceArray.append(chat_piece(chatmsg))
-    return data
-
-
-def chat_S2C(chatmsg):
-    data = STRADE_CHAT_S2C()
-    data.index = 1
-    data.chatType = 1
-    data.time = 1
-    data.chatData.CopyFrom(chat_data(chatmsg))
-    return data
-
-
 def chat_request(ctx, msg):
     req = SC2S_TRADE_CHANNEL_CHAT_REQ()
     req.ParseFromString(msg)
-    print(req)
-    chatmsg = req.chat.chatData.chatDataPieceArray[0].chatStr
+    
+    chat_type = req.chat.chatType
+    #target_account_id = req.chat.targetAccountId #currently unused
+    chat_str = req.chat.chatData.chatDataPieceArray[0].chatStr
+    uid = req.chat.chatData.chatDataPieceArray[0].chatDataPieceItem.uid
+    iid = req.chat.chatData.chatDataPieceArray[0].chatDataPieceItem.iid
+    pp_list = req.chat.chatData.chatDataPieceArray[0].chatDataPieceItem.pp
+
+    property_list = []
+    for pp in pp_list:
+        property_list.append(SCHATDATA_PIECE_ITEM_PROPERTY(pid=pp.pid, pv=pp.pv))
+
+    chat_piece_item_obj = SCHATDATA_PIECE_ITEM(uid=uid, iid=iid, pp=property_list)
+
+    chat_piece = SCHATDATA_PIECE()
+    chat_piece.chatStr = chat_str
+    chat_piece.chatDataPieceItem.CopyFrom(chat_piece_item_obj)
+
+    nickname = SACCOUNT_NICKNAME(originalNickName="Krofty", streamingModeNickName="")
+    chat_data = SCHATDATA()
+    chat_data.accountId = "1"
+    chat_data.characterId = "1"
+    chat_data.nickname.CopyFrom(nickname)
+    chat_data.partyId = "1"
+    chat_data.chatDataPieceArray.append(chat_piece)
+
+    chat_trade = STRADE_CHAT_S2C()
+    chat_trade.index = 1
+    chat_trade.chatType = chat_type
+    chat_trade.time = 1
+    chat_trade.chatData.CopyFrom(chat_data)
+
     return SS2C_TRADE_CHANNEL_CHAT_RES(
         result=pc.SUCCESS,
-        chats=[chat_S2C(chatmsg)]
+        chats=[chat_trade]
     )
 
 
