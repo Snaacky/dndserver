0bc64677e9ef14c58fac8d2242e774ee623cfdd6
james431987 <james431987@gmail.com>
2023-04-26 00:18:17 -0500

    all the logic for hwids and blocking banned users

--- dndserver/handlers/login.py
+++ dndserver/handlers/login.py
@@ -2,12 +2,30 @@ import random
 import string
 
 import argon2
-
+from datetime import datetime
 from dndserver.database import db
 from dndserver.models import Account
 from dndserver.protos.Account import SLOGIN_ACCOUNT_INFO, SS2C_ACCOUNT_LOGIN_RES, SC2S_ACCOUNT_LOGIN_REQ
 from dndserver.sessions import sessions
+from sqlalchemy.orm import Session
 
+
+def is_banned_hwid(hwid: str, session: Session) -> bool:
+    banned_hwid = session.query(Hwid).filter_by(hwid=hwid, is_banned=True).first()
+    return banned_hwid is not None
+
+def is_banned_user(username: str, session: Session) -> bool:
+    banned_user = session.query(Account).filter(Account.username == username, Account.ban_type != None).first()
+    return banned_user is not None
+
+def add_hwid_to_user(user_id: int, hwid: str, session: Session):
+    hwid_entry = Hwid(user_id=user_id, hwid=hwid, seen_at=datetime.now())
+    session.add(hwid_entry)
+    session.commit()
+
+def hwid_already_logged(user_id: int, hwid: str, session: Session) -> bool:
+    existing_hwid = session.query(Hwid).filter_by(user_id=user_id, hwid=hwid).first()
+    return existing_hwid is not None
 
 def process_login(ctx, msg):
     """Occurs when the user attempts to login to the game server."""
@@ -18,6 +36,17 @@ def process_login(ctx, msg):
     res = SS2C_ACCOUNT_LOGIN_RES(serverLocation=1)
 
     user = db.query(Account).filter_by(username=req.loginId).first()
+
+    # Check if the HWID is banned or the user is banned
+    if is_banned_hwid(req.hwIds[0], db) or is_banned_user(req.loginId, db):
+        if is_banned_hwid(req.hwIds[0], db):
+            #should be res.FAIL_LOGIN_BAN_HARDWARE but Not all SS2C_ACCOUNT_LOGIN_RES fields are implemented
+            res.Result = res.FAIL_PASSWORD
+        else:
+            #should be res.FAIL_LOGIN_BAN_USER but Not all SS2C_ACCOUNT_LOGIN_RES fields are implemented.
+            res.Result = res.FAIL_PASSWORD
+        return res
+    
     if not user:
         user = Account(
             username=req.loginId,
@@ -26,7 +55,7 @@ def process_login(ctx, msg):
         )
         user.save()
 
-        # TODO: Create new hwid objects and save them to the db here
+        add_hwid_to_user(user.id, req.hwIds[0], db) 
         res.secretToken = user.secret_token
 
     # Return FAIL_SHORT_ID_OR_PASSWORD on too short username/password.
@@ -46,10 +75,10 @@ def process_login(ctx, msg):
         res.Result = res.FAIL_PASSWORD
         return res
 
-    # Returns the respective SS2C_ACCOUNT_LOGIN_RES *__BAN_USER ban enum.
-    if user.ban_type:
-        res.Result = user.ban_type
-        return res
+    # Log HWID for the user after successful password verification
+    # if the combination of user_id and hwid doesn't already exist in the table
+    if not hwid_already_logged(user.id, req.hwIds[0], db):
+        add_hwid_to_user(user.id, req.hwIds[0], db)
 
     res.accountId = str(user.id)
     info = SLOGIN_ACCOUNT_INFO(AccountID=str(user.id))
