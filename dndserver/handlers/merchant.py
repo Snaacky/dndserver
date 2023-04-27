fcdf942cfaabfd23947acd05b0fd0a167ce4ae69
itzandroidtab <koen@itzandroidtab.nl>
2023-04-27 15:37:20 +0200

    reworked the merchant and added support for selling items

--- dndserver/handlers/merchant.py
+++ dndserver/handlers/merchant.py
@@ -1,4 +1,6 @@
 from dndserver.objects import items
+from dndserver.protos import PacketCommand as pc
+from dndserver.protos.Defines import Define_Message
 from dndserver.protos.Merchant import (
     SS2C_MERCHANT_LIST_RES,
     SMERCHANT_INFO,
@@ -9,9 +11,16 @@ from dndserver.protos.Merchant import (
     SMERCHANT_STOCK_SELL_BACK_ITEM_INFO,
     SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES,
 )
+from dndserver.data import merchant as Merchant
+
+
+def create_merchant(name, remaining_time):
+    """Helper function to create a merchant"""
+    return SMERCHANT_INFO(merchantId=name, faction=0, remainTime=remaining_time, isUnidentified=0)
 
 
 def get_merchant_list(ctx, msg):
+    """Occurs when the user opens the merchant menu"""
     # TODO: update make the timers update. In the data the faction and
     # isUnidentified is zero for all merchants. Check if this is correct
     # as well
@@ -20,22 +29,23 @@ def get_merchant_list(ctx, msg):
     # return all the merchants we have
     return SS2C_MERCHANT_LIST_RES(
         merchantList=[
-            create_surgeon(remaining_time),
-            create_santa(remaining_time),
-            create_woodsman(remaining_time),
-            create_tailor(remaining_time),
-            create_treasurer(remaining_time),
-            create_leathersmith(remaining_time),
-            create_armourer(remaining_time),
-            create_theCollector(remaining_time),
-            create_alchemist(remaining_time),
-            create_tavernMaster(remaining_time),
-            create_weaponsmith(remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Surgeon", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Santa", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Woodsman", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Tailor", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Treasurer", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Leathersmith", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Armourer", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_TheCollector", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Alchemist", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_TavernMaster", remaining_time),
+            create_merchant("DesignDataMerchant:Id_Merchant_Weaponsmith", remaining_time),
         ]
     )
 
 
 def get_buy_list(ctx, msg):
+    """Occurs when the user opens one of the merchant menus"""
     # To list the stock we need to send 3 messages.
     # 1 for starting, 1 with the actual data and the last for the end
     req = SC2S_MERCHANT_STOCK_BUY_ITEM_LIST_REQ()
@@ -44,13 +54,17 @@ def get_buy_list(ctx, msg):
     # TODO: handle every trader differently
 
     # send stage 1 (start)
-    ctx.reply(SS2C_MERCHANT_STOCK_BUY_ITEM_LIST_RES(result=1, loopMessageFlag=1, stockList=[]))
+    ctx.reply(
+        SS2C_MERCHANT_STOCK_BUY_ITEM_LIST_RES(
+            result=pc.SUCCESS, loopMessageFlag=Define_Message.LoopFlag.BEGIN, stockList=[]
+        )
+    )
 
     # send stage 2 (data the merchant is selling)
     ctx.reply(
         SS2C_MERCHANT_STOCK_BUY_ITEM_LIST_RES(
-            result=1,
-            loopMessageFlag=2,
+            result=pc.SUCCESS,
+            loopMessageFlag=Define_Message.LoopFlag.PROGRESS,
             stockList=[
                 SMERCHANT_STOCK_BUY_ITEM_INFO(
                     stockBuyId="DesignDataStockBuy:Id_StockBuy_Alchemist_01",
@@ -87,100 +101,40 @@ def get_buy_list(ctx, msg):
     )
 
     # let the protocol send stage 3
-    return SS2C_MERCHANT_STOCK_BUY_ITEM_LIST_RES(result=1, loopMessageFlag=3, stockList=[])
+    return SS2C_MERCHANT_STOCK_BUY_ITEM_LIST_RES(
+        result=pc.SUCCESS, loopMessageFlag=Define_Message.LoopFlag.END, stockList=[]
+    )
 
 
 def get_sellback_list(ctx, msg):
+    """Occurs when the user opens one of the merchant menus"""
     # To send the items we can sell back to the merchant we need to send 3 messages.
     # 1 for starting, 1 with the actual data and the last for the end
     req = SC2S_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_REQ()
     req.ParseFromString(msg)
 
-    # TODO: it looks like the game sends every item for every trader in this request (around 1000 items). It will do
-    # this every time we change trader. Not sure if this is intended or not. For example in the data of the Alchemist
-    # merchant it also sends the sellbacks for the weaponsmith.
-
     # send stage 1 (start)
-    ctx.reply(SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES(result=1, loopMessageFlag=1, stockList=[]))
-
-    # send stage 2 (data the merchant is selling)
     ctx.reply(
         SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES(
-            result=1,
-            loopMessageFlag=2,
-            stockList=[
-                SMERCHANT_STOCK_SELL_BACK_ITEM_INFO(
-                    stockSellBackId="DesignDataStockSellBack:Id_StockSellBack_Weaponsmith", stockUniqueId=215
-                )
-            ],
+            result=pc.SUCCESS, loopMessageFlag=Define_Message.LoopFlag.BEGIN, stockList=[]
         )
     )
 
+    sellback = SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES(
+        result=pc.SUCCESS, loopMessageFlag=Define_Message.LoopFlag.PROGRESS
+    )
+
+    # TODO: it looks like the game sends every item for every trader in this request (around 1200 items). It will do
+    # this every time we change trader. Not sure if this is intended or not. For example in the data of the Alchemist
+    # merchant it also sends the sellbacks for the weaponsmith.
+    for merchant, item_store_ids in zip(Merchant.sellback.keys(), Merchant.sellback.values()):
+        for id in item_store_ids:
+            sellback.stockList.append(SMERCHANT_STOCK_SELL_BACK_ITEM_INFO(stockSellBackId=merchant, stockUniqueId=id))
+
+    # send stage 2 (data the merchant is selling)
+    ctx.reply(sellback)
+
     # let the protocol send stage 3
-    return SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES(result=1, loopMessageFlag=3, stockList=[])
-
-
-def create_surgeon(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Surgeon", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_santa(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Santa", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_woodsman(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Woodsman", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_tailor(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Tailor", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_treasurer(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Treasurer", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_leathersmith(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Leathersmith", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_armourer(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Armourer", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_theCollector(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_TheCollector", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_alchemist(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Alchemist", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_tavernMaster(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_TavernMaster", faction=0, remainTime=remaining_time, isUnidentified=0
-    )
-
-
-def create_weaponsmith(remaining_time) -> SMERCHANT_INFO:
-    return SMERCHANT_INFO(
-        merchantId="DesignDataMerchant:Id_Merchant_Weaponsmith", faction=0, remainTime=remaining_time, isUnidentified=0
+    return SS2C_MERCHANT_STOCK_SELL_BACK_ITEM_LIST_RES(
+        result=pc.SUCCESS, loopMessageFlag=Define_Message.LoopFlag.END, stockList=[]
     )
