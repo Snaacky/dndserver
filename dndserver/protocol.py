32d4a5233764230aa2ae5b2eb200b088e3efe8fc
Filippo Leone @North <fleonen@gmail.com>
2023-04-28 15:52:02 +0200

    Updated protocol with missing lobby methods + ingame

--- dndserver/protocol.py
+++ dndserver/protocol.py
@@ -3,7 +3,7 @@ import struct
 from loguru import logger
 from twisted.internet.protocol import Factory, Protocol
 
-from dndserver.handlers import character, friends, lobby, login, trade, menu, merchant, party, ranking
+from dndserver.handlers import character, friends, lobby, login, trade, menu, merchant, party, ranking, ingame
 from dndserver.objects.user import User
 from dndserver.protos import PacketCommand as pc
 from dndserver.sessions import sessions
@@ -54,6 +54,9 @@ class GameProtocol(Protocol):
                 pc.C2S_CLASS_LEVEL_INFO_REQ: character.get_experience,
                 pc.C2S_INVENTORY_SINGLE_UPDATE_REQ: character.move_item,
                 pc.C2S_LOBBY_ENTER_REQ: lobby.enter_lobby,
+                pc.C2S_LOBBY_GAME_DIFFICULTY_SELECT_REQ: lobby.map_select,
+                pc.C2S_LOBBY_REGION_SELECT_REQ: lobby.region_select,
+                pc.C2S_OPEN_LOBBY_MAP_REQ: lobby.open_lobby_map,
                 pc.C2S_CHARACTER_SELECT_ENTER_REQ: lobby.enter_character_select,
                 pc.C2S_FRIEND_LIST_ALL_REQ: friends.list_friends,
                 pc.C2S_FRIEND_FIND_REQ: friends.find_user,
@@ -66,6 +69,10 @@ class GameProtocol(Protocol):
                 pc.C2S_TRADE_MEMBERSHIP_REQUIREMENT_REQ: trade.get_trade_reqs,
                 pc.C2S_TRADE_MEMBERSHIP_REQ: trade.process_membership,
                 pc.C2S_RANKING_RANGE_REQ: ranking.get_ranking,
+                pc.C2S_AUTO_MATCH_REG_REQ: ingame.matchmaking,
+                pc.C2S_GATHERING_HALL_CHANNEL_LIST_REQ: ingame.gathering_hall_channel_list,
+                pc.C2S_GATHERING_HALL_CHANNEL_SELECT_REQ: ingame.gathering_hall_select_channel,
+                pc.C2S_GATHERING_HALL_TARGET_EQUIPPED_ITEM_REQ: ingame.gathering_hall_equip,
             }
             handler = [k for k in handlers.keys() if k == _id]
             if not handler:
