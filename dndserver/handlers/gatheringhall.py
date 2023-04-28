58b04f1bc4b7e05bb573e15812fdbf3031b6840f
Filippo Leone @North <fleonen@gmail.com>
2023-04-28 16:55:31 +0200

    added mock data

--- dndserver/handlers/gatheringhall.py
+++ dndserver/handlers/gatheringhall.py
@@ -2,28 +2,30 @@ from dndserver.database import db
 from dndserver.protos.GatheringHall import (
     SC2S_GATHERING_HALL_CHANNEL_LIST_REQ, SS2C_GATHERING_HALL_CHANNEL_LIST_RES,
     SC2S_GATHERING_HALL_CHANNEL_SELECT_REQ, SS2C_GATHERING_HALL_CHANNEL_SELECT_RES,
-    SC2S_GATHERING_HALL_TARGET_EQUIPPED_ITEM_REQ, SS2C_GATHERING_HALL_TARGET_EQUIPPED_ITEM_RES
+    SC2S_GATHERING_HALL_TARGET_EQUIPPED_ITEM_REQ, SS2C_GATHERING_HALL_TARGET_EQUIPPED_ITEM_RES,
+    SGATHERING_HALL_CHANNEL
     )
-
+from dndserver.protos import PacketCommand as pc
 from dndserver.sessions import sessions
 from dndserver.handlers import character
 
 def gathering_hall_channel_list(ctx, msg):
     req = SC2S_GATHERING_HALL_CHANNEL_LIST_REQ()
     req.ParseFromString(msg)
-    channel = SGATHERING_HALL_CHANNEL(
-        channelIndex=sessions[ctx.transport].gatheringhall.channel_index,
-        channelId=sessions[ctx.transport].gatheringhall.channel_index,
-        memberCount=sessions[ctx.transport].party.players,
-        groupIndex=sessions[ctx.transport].gatheringhall.channel_index
-    )
-    res = SS2C_GATHERING_HALL_CHANNEL_LIST_RES(channels=channel)
+    channel = []
+    res = SS2C_GATHERING_HALL_CHANNEL_LIST_RES()
+    for ch in range(1,7):
+        res.channels.append(SGATHERING_HALL_CHANNEL(
+            channelIndex=ch,
+            channelId=f'{ch}',
+            memberCount=10,
+            groupIndex=ch
+        ))
     return res
 
 def gathering_hall_select_channel(ctx, msg):
     req = SC2S_GATHERING_HALL_CHANNEL_SELECT_REQ()
     req.ParseFromString(msg)
-    sessions[ctx.transport].gatheringhall.channel_index = req.channelIndex
     res = SS2C_GATHERING_HALL_CHANNEL_SELECT_RES(result=pc.SUCCESS)
     return res
 
