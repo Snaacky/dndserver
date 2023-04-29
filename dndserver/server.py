80ccccb7e2058003e3c3b80f2f05cf372be8f455
Snaacky <snaacky@pm.me>
2023-04-28 20:52:19 -0400

    Server will no longer run without a database present (#78)

--- dndserver/server.py
+++ dndserver/server.py
@@ -1,16 +1,19 @@
 import asyncio
+import os
 
 from loguru import logger
 from twisted.internet import reactor
 
-from dndserver.database import engine
-from dndserver.models import base
 from dndserver.config import config
 from dndserver.protocol import GameFactory
 
 
 async def main():
     """Entrypoint where the server first initializes"""
+    # Stop the server from executing if the database is missing.
+    if not os.path.isfile("dndserver.db"):
+        logger.error("dndserver.db doesn't exist, did you run alembic upgrade head?")
+        raise SystemExit
 
     # Sets up the factory for the game server traffic.
     tcpFactory = GameFactory()
