1c6b5acdcc13a2be5c69ad75e7dfdb4735eb1da3
billze <billman30@hotmail.com>
2023-05-05 09:10:47 +1000

    Added ranger perks missing

--- dndserver/data/perks.py
+++ dndserver/data/perks.py
@@ -56,11 +56,11 @@ ranger = [
     "DesignDataPerk:Id_Perk_EnhancedHearing",
     "DesignDataPerk:Id_Perk_Kinesthesia",
     "DesignDataPerk:Id_Perk_NimbleHands",
-    "DesignDataPerk:Id_Perk_RangedWeaponsExpert",
+    "DesignDataPerk:Id_Perk_RangedWeaponsMastery",
     "DesignDataPerk:Id_Perk_Sharpshooter",
     "DesignDataPerk:Id_Perk_SpearProficiency",
-    "DesignDataPerk:Id_Perk_Tracking",
-    "DesignDataPerk:Id_Perk_TrapExpert",
+    "DesignDataPerk:Id_Perk_Chase",
+    "DesignDataPerk:Id_Perk_TrapMastery",
     "DesignDataPerk:Id_Perk_CripplingShot",
     "DesignDataPerk:Id_Perk_QuickReload",
 ]
@@ -105,7 +105,7 @@ perks = {
 }
 
 # Other items. Not used at the moment.
-# DesignDataPerk:Id_Perk_Chase
+# DesignDataPerk:Id_Perk_Tracking
 # DesignDataPerk:Id_Perk_DaggerMastery
 # DesignDataPerk:Id_Perk_LockpickSet
 # DesignDataPerk:Id_Perk_Backstab
@@ -113,8 +113,8 @@ perks = {
 # DesignDataPerk:Id_Perk_Malice
 # DesignDataPerk:Id_Perk_Toughness
 # DesignDataPerk:Id_Perk_Smash
-# DesignDataPerk:Id_Perk_RangedWeaponsMastery
+# DesignDataPerk:Id_Perk_RangedWeaponsExpert
 # DesignDataPerk:Id_Perk_ShieldMastery
 # DesignDataPerk:Id_Perk_DefenseMastery
 # DesignDataPerk:Id_Perk_SurvivalistTongue
-# DesignDataPerk:Id_Perk_TrapMastery
+# DesignDataPerk:Id_Perk_TrapExpert
