cdce716089e82c8f7258d6e619df61a261ff021b
itzandroidtab <koen@itzandroidtab.nl>
2023-05-05 15:34:19 +0200

    added handlers for inventory movement at the merchant

--- dndserver/handlers/inventory.py
+++ dndserver/handlers/inventory.py
@@ -7,6 +7,14 @@ from dndserver.protos.Inventory import (
     SC2S_INVENTORY_SINGLE_UPDATE_REQ,
     SS2C_INVENTORY_MOVE_RES,
     SS2C_INVENTORY_SINGLE_UPDATE_RES,
+    SC2S_INVENTORY_MERGE_REQ,
+    SC2S_INVENTORY_SPLIT_MOVE_REQ,
+    SC2S_INVENTORY_SWAP_REQ,
+    SC2S_INVENTORY_SPLIT_MERGE_REQ,
+    SS2C_INVENTORY_MERGE_RES,
+    SS2C_INVENTORY_SPLIT_MOVE_RES,
+    SS2C_INVENTORY_SWAP_RES,
+    SS2C_INVENTORY_SPLIT_MERGE_RES,
 )
 
 
@@ -29,16 +37,19 @@ def get_all_items(character_id, inventory_id=None):
     return ret
 
 
-def delete_item(character_id, item):
+def delete_item(character_id, item, is_query=False):
     """Helper function to remove a item from a character"""
-    query = db.query(Item).filter_by(character_id=character_id).filter_by(id=item.itemUniqueId).first()
+    if is_query:
+        query = item
+    else:
+        query = db.query(Item).filter_by(character_id=character_id).filter_by(id=item.itemUniqueId).first()
 
     # check if we have the item
     if query is None:
         return False
 
     # delete all the attributes the item has
-    attributes = db.query(ItemAttribute).filter_by(item_id=item.itemId).all()
+    attributes = db.query(ItemAttribute).filter_by(item_id=query.id).all()
     for attribute in attributes:
         attribute.delete()
 
@@ -66,13 +77,16 @@ def get_stack_limit(item):
         return 3
     elif "Id_Item_GoldCoinPurse" in item or "Id_Item_GoldCoinChest" in item or "Id_Item_GoldCoinBag" in item:
         return 1
-    elif "Id_Item_GoldCoins" or "Id_Item_SilverCoin" in item:
+    elif "Id_Item_GoldCoins" in item or "Id_Item_SilverCoin" in item:
         return 10
+    elif "Id_Item_Arrow" in item:
+        return 15
     else:
         return 0
 
 
 def get_inv_limit(item):
+    """Helper function to get the max inventory count for a item"""
     # TODO: get real values for limits
     if "Id_Item_GoldCoinPurse" in item:
         return 50
@@ -84,7 +98,191 @@ def get_inv_limit(item):
         return 0
 
 
-def move_item(ctx, msg):
+def split_item(from_item, item_id, to_inventory, to_slot, quantity, character_id):
+    """Helper function to split a item and generate a new item at the provided location"""
+    it = Item()
+    it.character_id = character_id
+    it.inventory_id = to_inventory
+    it.slot_id = to_slot
+    it.quantity = quantity
+    attributes = list()
+
+    # check how we should split the item
+    if get_inv_limit(from_item.item_id):
+        # move item out of purse. decrease the amount of items in the inventory. The amount
+        # of items we need to remove is stored in the item count
+        from_item.inv_count -= quantity
+
+        # extract the item out of the inventory
+        it.item_id = item_id
+
+    else:
+        # The new item is not in the database. This means it has split. decrement the old item
+        from_item.quantity -= quantity
+
+        # create a new item in the database
+        it.item_id = from_item.item_id
+
+        # copy all the attributes
+        item_attr = db.query(ItemAttribute).filter_by(item_id=from_item.id).all()
+        for attribute in item_attr:
+            attr = ItemAttribute()
+            attr.item_id = attribute.item_id
+            attr.primary = attribute.primary
+            attr.property = attribute.property
+            attr.value = attribute.value
+            attributes.append(attr)
+
+    # add the item ot the database
+    add_item((it, attributes))
+
+    # update the unique id to notify the client what id to use
+    return it.id
+
+
+def merge_items(from_item, to_item, amount, character_id, from_is_persistent=True):
+    """Helper function to merge two items."""
+    # Check how we can merge the two items
+    if get_inv_limit(from_item.item_id) and get_inv_limit(to_item.item_id):
+        # we have two items that have a inventory. Merge the to items together by
+        # moving the inventory from one to the other
+        limit = get_inv_limit(to_item.item_id)
+        total = to_item.inv_count + amount
+
+        # move the inventory of the target until the limit
+        to_item.inv_count = min(total, limit)
+        from_item.inv_count -= amount if total <= limit else total - limit
+
+        # do a early return as we should not delete the item we moved from
+        return True
+
+    elif get_inv_limit(to_item.item_id):
+        # the target is a item with a inventory. Move the quantity into the target
+        limit = get_inv_limit(to_item.item_id)
+        total = to_item.inv_count + amount
+
+        # move the quantity to the target until the limit
+        to_item.inv_count = min(total, limit)
+        from_item.quantity -= amount if total <= limit else total - limit
+
+    elif get_stack_limit(to_item.item_id):
+        # the target is a item that can hold a stack. Move the item(s) to the target
+        limit = get_stack_limit(to_item.item_id)
+        total = to_item.quantity + amount
+
+        # move the items in the stack
+        to_item.quantity = min(total, limit)
+        from_item.quantity -= amount if total <= limit else total - limit
+
+    else:
+        # we cannot merge the two items. Return invalid
+        return False
+
+    # check if we should delete the old item
+    if from_item.quantity <= 0 and from_is_persistent:
+        delete_item(character_id, from_item, True)
+
+    return True
+
+
+def merge_request(ctx, msg):
+    """Occurs when the user drags an item on another in the merchant screen."""
+    req = SC2S_INVENTORY_MERGE_REQ()
+    req.ParseFromString(msg)
+
+    # get the character
+    character = sessions[ctx.transport].character
+
+    # get both items we want to merge
+    old = db.query(Item).filter_by(character_id=character.id).filter_by(id=req.srcInfo.uniqueId).first()
+    new = db.query(Item).filter_by(character_id=character.id).filter_by(id=req.dstInfo.uniqueId).first()
+
+    # get the amount we want to merge
+    amount = new.inv_count if new.inv_count > 0 else new.quantity
+
+    # merge the items
+    merge_items(old, new, amount, character.id)
+
+    return SS2C_INVENTORY_MERGE_RES()
+
+
+def split_move_request(ctx, msg):
+    """Occurs when the user wants to split an item in the merchant screen."""
+    req = SC2S_INVENTORY_SPLIT_MOVE_REQ()
+    req.ParseFromString(msg)
+
+    # get both items we want to merge
+    item = (
+        db.query(Item)
+        .filter_by(character_id=sessions[ctx.transport].character.id)
+        .filter_by(id=req.srcInfo.uniqueId)
+        .first()
+    )
+
+    res = SS2C_INVENTORY_SPLIT_MOVE_RES()
+    res.newUniqueId = split_item(
+        item, item.item_id, req.dstInventoryId, req.dstSlotId, req.count, sessions[ctx.transport].character.id
+    )
+    res.newInventoryId = req.dstInventoryId
+    res.newSlotId = req.dstSlotId
+
+    return res
+
+
+def swap_request(ctx, msg):
+    """Occurs when the user wants to swap two items in the merchant screen."""
+    req = SC2S_INVENTORY_SWAP_REQ()
+    req.ParseFromString(msg)
+
+    # get the character
+    character = sessions[ctx.transport].character
+
+    # move the main item to the new location
+    it = db.query(Item).filter_by(character_id=character.id).filter_by(id=req.srcInfo.uniqueId).first()
+    it.inventory_id = req.dstInfo.inventoryId
+    it.slot_id = req.dstInfo.slotId
+
+    # move all the other items
+    for item in req.swapInfos:
+        it = db.query(Item).filter_by(character_id=character.id).filter_by(id=item.dstInfo.uniqueId).first()
+        it.inventory_id = item.newInventoryId
+        it.slot_id = item.newSlotId
+
+    return SS2C_INVENTORY_SWAP_RES()
+
+
+def split_merge_request(ctx, msg):
+    """Occurs when the user wants to split and merge the result on another item in the merchant screen."""
+    req = SC2S_INVENTORY_SPLIT_MERGE_REQ()
+    req.ParseFromString(msg)
+
+    # get the character
+    character = sessions[ctx.transport].character
+
+    # get the item we want to split from and the item we want to merge to
+    old = db.query(Item).filter_by(character_id=character.id).filter_by(id=req.srcInfo.uniqueId).first()
+    new = db.query(Item).filter_by(character_id=character.id).filter_by(id=req.dstInfo.uniqueId).first()
+
+    # create a temporary item with the paramerers for the merge. No need to do both a full split and a merge
+    it = Item()
+    it.item_id = old.item_id
+    it.ammo_count = old.ammo_count
+
+    # check if the item is a inventory item
+    if get_inv_limit(it.item_id):
+        it.inv_count = req.count
+        old.inv_count -= req.count
+    else:
+        it.quantity = req.count
+        old.quantity -= req.count
+
+    # merge the two items.
+    merge_items(it, new, req.count, character.id, False)
+
+    return SS2C_INVENTORY_SPLIT_MERGE_RES()
+
+
+def move_request(ctx, msg):
     """Occurs when the user moves an item at a merchant"""
     req = SC2S_INVENTORY_MOVE_REQ()
     req.ParseFromString(msg)
@@ -102,7 +300,7 @@ def move_item(ctx, msg):
     return SS2C_INVENTORY_MOVE_RES()
 
 
-def move_single_item(ctx, msg):
+def move_single_request(ctx, msg):
     """Occurs when the user moves an item in the stash (only in the stash screen)"""
     req = SC2S_INVENTORY_SINGLE_UPDATE_REQ()
     req.ParseFromString(msg)
@@ -130,103 +328,30 @@ def move_single_item(ctx, msg):
 
             continue
 
-        # Not a simple move, search for the item we are updating to
-        new_query = db.query(Item).filter_by(character_id=character.id).filter_by(id=new.itemUniqueId).first()
+        # Not a simple move, search for the item we are updating to (specific search as the unique id the
+        # game gives can be random and already in use for another item)
+        new_query = (
+            db.query(Item)
+            .filter_by(character_id=character.id)
+            .filter_by(id=new.itemUniqueId)
+            .filter_by(slot_id=new.slotId)
+            .filter_by(inventory_id=new.inventoryId)
+            .filter_by(item_id=new.itemId)
+            .first()
+        )
 
         # check if we have both items in the database
         if new_query is None:
-            it = Item()
-            it.character_id = old_query.character_id
-            it.quantity = new.itemCount
-            it.inventory_id = new.inventoryId
-            it.slot_id = new.slotId
-
-            attributes = list()
-
-            if get_inv_limit(old.itemId):
-                # move item out of purse. decrease the amount of items in the inventory. The amount
-                # of items we need to remove is stored in the item count
-                old_query.inv_count -= new.itemCount
-
-                # extract the item out of the inventory
-                it.item_id = new.itemId
-
-            else:
-                # The new item is not in the database. This means it has split. decrement the old item
-                old_query.quantity -= old.itemCount
-
-                # create a new item in the database
-                it.item_id = old.itemId
-
-                # copy all the attributes
-                item_attr = db.query(ItemAttribute).filter_by(item_id=old.itemUniqueId).all()
-                for attribute in item_attr:
-                    attr = ItemAttribute()
-                    attr.item_id = attribute.item_id
-                    attr.primary = attribute.primary
-                    attr.property = attribute.property
-                    attr.value = attribute.value
-
-                    attributes.append(attr)
-
-            # add the item ot the database
-            add_item((it, attributes))
-
-            # update the unique id to notify the client what id to use
-            new.itemUniqueId = it.id
-
+            # no new item. Split the old item into the new item. Set unique id of the new item
+            new.itemUniqueId = split_item(
+                old_query, new.itemId, new.inventoryId, new.slotId, new.itemCount, character.id
+            )
         else:
-            # check what action to do
-            if get_inv_limit(old.itemId) and get_inv_limit(new.itemId):
-                # move item from one purse to another
-                total_count = new_query.inv_count + new.itemContentsCount
-                inventory_limit = get_inv_limit(new.itemId)
+            # get the amount we want to merge
+            amount = new.itemContentsCount if new.itemContentsCount > 0 else new.itemCount
 
-                # limit the amount of items in the inventory
-                if total_count > inventory_limit:
-                    # we have a overflow. Increase the amount on the new item till it does not fit anymore
-                    old_query.inv_count = total_count - inventory_limit
-                    new_query.inv_count = inventory_limit
-                else:
-                    old_query.inv_count = 0
-                    new_query.inv_count = total_count
-
-            elif get_inv_limit(new.itemId):
-                # move item into purse. Put the item in the inventory and delete the other one if everything fits
-                total_count = new_query.inv_count + old.itemCount
-                inventory_limit = get_inv_limit(new.itemId)
-
-                # limit the amount of items in the inventory
-                if total_count > inventory_limit:
-                    # we have a overflow. Keep the old item and decrease it. Increase the amount on the new item
-                    old_query.quantity = total_count - inventory_limit
-                    new_query.inv_count = inventory_limit
-
-                else:
-                    new_query.inv_count = total_count
-
-                    # delete the item
-                    delete_item(character.id, old)
-
-            elif get_stack_limit(new.itemId):
-                # we have both items. Merge them
-                total_count = old.itemCount + new.itemCount
-                stack_limit = get_stack_limit(new.itemId)
-
-                # limit the amount of items in a stack. The game expects this and wont update otherwise
-                if total_count > stack_limit:
-                    # we have a overflow. Keep the old item and decrease it. Increase the amount on the new item
-                    old_query.quantity = total_count - stack_limit
-                    new_query.quantity = stack_limit
-
-                else:
-                    # Everything fits in the new item. Merge and delete the old item
-                    new_query.quantity = total_count
-
-                    # delete the item
-                    delete_item(character.id, old)
-
-            else:
+            # we have a merge
+            if merge_items(old_query, new_query, amount, character.id) is False:
                 # return a error as we cannot process this request
                 return SS2C_INVENTORY_SINGLE_UPDATE_RES(
                     result=pc.FAIL_NO_VALUE, oldItem=req.oldItem, newItem=req.newItem
